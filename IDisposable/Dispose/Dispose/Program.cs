d1b6c9a24ab010da1cb6da24c594028c37780da7
luisdanielse <luisdaniel.se@gmail.com>
2021-07-30 08:17:29 -0600

    Adding IDisposable to realease resources that might be used by future methods

---
+++ IDisposable/Dispose/Dispose/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathFile = @"E:\visual-studiop\C#July2021\IDisposable\Dispose\Dispose\disposable.txt";
+
+            Console.WriteLine("Press any key to create the file");
+            Console.ReadKey();
+
+            /*
+            var myWriter = new WriteFile(pathFile);
+            myWriter.write("the content of the file");
+            myWriter.write("more content in the file");
+            Console.WriteLine("File created. Press any key to exit");
+            Console.ReadKey();
+
+            var myWriter = new WriteFile(pathFile);
+            var myWriter2 = new WriteFile(pathFile);
+            myWriter.write("the content of the file");
+            myWriter2.write("more content in the file");
+            Console.WriteLine("File created. Press any key to exit");
+            Console.ReadKey();
+            */
+
+        /* The using statement will look for the Dispose method
+         * (declared by the interface IDisposable )*/
+        using(var myWriter = new WriteFile(pathFile))
+        {
+            myWriter.write("This is the content of the file");
+    
+        }
+        using (var myWriter2 = new WriteFile(pathFile))
+        {
+            myWriter2.write("more content in the file");
+
+        }
+
+            Console.WriteLine("File created. Press any key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ IDisposable/Dispose/Dispose/WriteFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispose
+{
+    class WriteFile: IDisposable
+    {
+        private string _path;
+        private StreamWriter _file;
+
+        public WriteFile(string path)
+        {
+            _path = path;
+            _file = new StreamWriter(path);
+        }
+
+        /* The dispose method is a method we MUST implement
+         * because of the IDisposable interface */
+        public void Dispose()
+        {
+            _file.Close();
+            Console.WriteLine("Dispose executed by the using statement");
+        }
+
+        public void write(string text)
+        {
+            _file.WriteLine(text);
+        }
+    
+    }
+}
